ff550eef08173cd24bd01a0d8d35cbc36ac1206d
YJC <424586730@qq.com>
2020-11-07 00:08:14 +0800

    fix

--- helloSeiral/Main.py
+++ helloSeiral/Main.py
@@ -1,7 +1,6 @@
 import sys
 import time
 
-from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QIcon, QTextCursor
 from PyQt5.QtWidgets import QApplication, QMessageBox, QDialog, QWidget
 from PyQt5.QtWidgets import QMainWindow
@@ -11,7 +10,31 @@ from mainwindow import Ui_MainWindow  # 加载我们的布局
 from dialog import Ui_AboutRobot
 import mySerial
 import threading
-
+import matplotlib
+matplotlib.use("Qt5Agg")  # 声明使用QT5
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+#创建一个matplotlib图形绘制类
+class MyFigure(FigureCanvas):
+    def __init__(self,width=5, height=4, dpi=100):
+        #第一步：创建一个创建Figure
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        #第二步：在父类中激活Figure窗口
+        super(MyFigure,self).__init__(self.fig) #此句必不可少，否则不能显示图形
+        #第三步：创建一个子图，用于绘制图形用，111表示子图编号，如matlab的subplot(1,1,1)
+        self.axes = self.fig.add_subplot(111)
+    #第四步：就是画图，【可以在此类中画，也可以在其它类中画】
+    def plotsin(self):
+        self.axes0 = self.fig.add_subplot(111)
+        t = np.arange(0.0, 3.0, 0.01)
+        s = np.sin(2 * np.pi * t)
+        self.axes0.plot(t, s)
+    def plotcos(self):
+        t = np.arange(0.0, 3.0, 0.01)
+        s = np.sin(2 * np.pi * t)
+        self.axes.plot(t, s)
 
 class ErrorMessage():
     def __init__(self, qWindow):
