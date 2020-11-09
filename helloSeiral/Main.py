ad924ff6ef021b9926c6c856182921ad04ce37e2
YJC <424586730@qq.com>
2020-11-09 19:19:20 +0800

    聪聪串口助手beta版0.2

--- helloSeiral/Main.py
+++ helloSeiral/Main.py
@@ -1,15 +1,15 @@
 import sys
 import time
 
-from PyQt5.QtGui import QTextCursor
+import numpy as np
 from PyQt5.QtWidgets import QApplication, QMessageBox, QDialog, QWidget
 from PyQt5.QtWidgets import QMainWindow
 from mainwindow import Ui_MainWindow  # 加载我们的布局
-from dialog import Ui_AboutRobot
-import mySerial
 from AboutDiaglog import AboutDialog
 
 from pyQTInit import PyQtInitialization
+
+import pyqtgraph.opengl as gl
 
 
 class UsingTest(QMainWindow, Ui_MainWindow):
@@ -19,48 +19,82 @@ class UsingTest(QMainWindow, Ui_MainWindow):
         self.setupUi(self)  # 初始化ui
         self.init = PyQtInitialization(self)
 
+    def draw3D(self):
+        pass
+
+    def updateText(self):
+        self.rollText.setText('%0.1f°' % (self.rollData[-1]))
+        self.pitchText.setText('%0.1f°' % (self.pitchData[-1]))
+        self.yawText.setText('%0.1f°' % (self.yawData[-1]))
+
 
     def drawFigure(self):
-        if (type(self.mpuData) == Exception):
+        if issubclass(type(self.mpuData),Exception):
             self.drawTextBrowser.append('出错了!!' + '这可能是由于你传输了错误的8050帧格式')
-        try:
-        # print(mpuData)
+        if self.mpuData:
+            self.curRollAngle = self.mpuData[-3]
+
+            sub = self.curRollAngle - self.rollAngle
+            # 正数代表要向正方转sub度
+            if sub > 0:
+                self.positiveAxis.rotate(sub, 1, 0, 0)
+                self.negativeAxis.rotate(sub, -1, 0, 0)
+            else:
+                self.positiveAxis.rotate(sub, -1, 0, 0)
+                self.negativeAxis.rotate(sub, 1, 0, 0)
+                pass
             self.rollData[:-1] = self.rollData[1:]
-            self.rollData[-1] = self.mpuData[-3]
+            self.rollData[-1] = self.curRollAngle
+            # print(self.rollData[-1])
+            # self.positiveAxis.rotate(self.rollData[-1], 1, 0, 0)
+            # self.negativeAxis.rotate(self.rollData[-1], 0, 0, 0)
+            lastIndex = 100
             self.curveRoll.setData(self.rollData)
-            self.curveRoll.setPos(self.Index, 0)
+            self.curveRoll.setPos(lastIndex, 0)
+
+            self.rollText.setPos(lastIndex, self.rollData[-1])
+            self.rollArrow.setPos(lastIndex, self.rollData[-1])
+
+            self.curvePitch.setData(self.pitchData)
+            self.curvePitch.setPos(lastIndex, 0)
+            self.pitchText.setPos(lastIndex, self.pitchData[-1])
+            self.pitchArrow.setPos(lastIndex, self.pitchData[-1])
+
+            self.curveYaw.setData(self.yawData)
+            self.curveYaw.setPos(lastIndex, 0)
+            self.yawText.setPos(lastIndex, self.yawData[-1])
+            self.yawArrow.setPos(lastIndex, self.yawData[-1])
+
+
 
             self.pitchData[:-1] = self.pitchData[1:]
             self.pitchData[-1] = self.mpuData[-2]
             self.curvePitch.setData(self.pitchData)
-            self.curvePitch.setPos(self.Index, 0)
+            self.curvePitch.setPos(lastIndex, 0)
 
             self.yawData[:-1] = self.yawData[1:]
             self.yawData[-1] = self.mpuData[-1]
             self.curveYaw.setData(self.yawData)
-            self.curveYaw.setPos(self.Index, 0)
-            self.Index += .01
-            time.sleep(.1)
+            self.curveYaw.setPos(lastIndex, 0)
 
-
-        except Exception as e:
-            self.debugtextBrowser.append(str(e))
+            time.sleep(.01)
 
     def getMCU8050Data(self):
-        print('sb')
-        code=getattr(self,'data',1)
+
+        code = getattr(self, 'data', 1)
         while (code == 1):
             code = getattr(self, 'data', 1)
             self.painterTextBrowser.append('data错误data错误')
         self.painterTextBrowser.append('数据流一切正常,给我冲')
         while self.open:
-            if self.acceptedData==2:
-                if(self.draw):
+            if self.acceptedData == 2:
+                if (self.draw):
                     self.mpuData = self.myPort.decodeMPU6050(self.data)
-                    if(type(self.mpuData)==Exception):
-                        self.errorMessageBox.warning(self,'mpudata错误','mpudata错误mpudata错误mpudata错误')
+                    if (type(self.mpuData) == Exception):
+                        self.errorMessageBox.warning(self, 'mpudata错误', 'mpudata错误mpudata错误mpudata错误')
                     else:
                         self.drawFigure()
+                        self.draw3D()
                 else:
                     self.painterTextBrowser.append('绘图已中断,开始等待')
                     time.sleep(1)
@@ -68,6 +102,7 @@ class UsingTest(QMainWindow, Ui_MainWindow):
             else:
                 self.painterTextBrowser.append('数据流已中断,正在重试,1s后再次检测!!!')
                 time.sleep(1)
+
     def sendData2MyPort(self):
         if self.open:
             self.sendtext = self.sendedRegion.toPlainText()
@@ -77,8 +112,6 @@ class UsingTest(QMainWindow, Ui_MainWindow):
             self.debugtextBrowser.append("你发送了 {}颗小心心 ♥(ˆ◡ˆԅ)❤".format(sendCount))
         else:
             self.errorMessageBox.warning('请先打开串口', '打开串口先哦')
-
-
 
     def aboutDialog(self):
         about = AboutDialog()
@@ -96,8 +129,8 @@ class UsingTest(QMainWindow, Ui_MainWindow):
             pass
         else:
             self.myPort.ser.close()
-        self.open=0
-        self.draw=0
+        self.open = 0
+        self.draw = 0
 
 
 if __name__ == '__main__':  # 程序的入口
--- helloSeiral/pyQTInit.py
+++ helloSeiral/pyQTInit.py
@@ -1,15 +1,23 @@
 import threading
-import pyqtgraph as pg
-from PyQt5.QtGui import QIcon
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QIcon, QVector3D
+from PyQt5.QtWidgets import QMainWindow
+from mainwindow import Ui_MainWindow  # 加载我们的布局
+
 from pyQtErrorMessage import ErrorMessage
 import numpy as np
 import mySerial
 from pyQtSignal import Signal
 from pyQtListener import Listener
+import pyqtgraph.opengl as gl
+import pyqtgraph as pg
 
-class PyQtInitialization():
-    def __init__(this,self):
-        this.self=self
+
+class PyQtInitialization(QMainWindow, Ui_MainWindow):
+    def __init__(this, self):
+        super(PyQtInitialization, this).__init__()
+        this.self = self
         this.initSingals(self)
         this.initVar(self)
         this.initCombox(self)
@@ -17,33 +25,100 @@ class PyQtInitialization():
         this.initListener(self)
         this.initIcon(self)
         this.figureInit(self)
+        this.init3D(self)
         # this.autoCheckBox(self)
         this.initMain(self)
-    def initListener(this,self):
-        self.listener=Listener(self)
-    def initSingals(this,self):
-        self.signals=Signal(self)
-    def figureInit(this,self):
-        self.rollDrawing=pg.PlotWidget()
-        self.Index=0
-        self.rollData=np.random.normal(size=100)
-        self.curveRoll=self.rollDrawing.plot(self.rollData)
 
+    def init3D(this, self):
+        self.window3D = gl.GLViewWidget()
+        self.window3D.opts['distance'] = 4
+
+        # ax.plot_surface(x, y, z, color='b')
+
+        ## create three grids, add each to the view
+        self.positiveAxis = gl.GLAxisItem(QVector3D(10, 10, 10), )
+        self.negativeAxis = gl.GLAxisItem(QVector3D(10, 10, 10), )
+        # xgrid.setSize(x=10)
+
+        # zgrid = gl.GLAxisItem(QVector3D(10,10,10))
+        self.window3D.addItem(self.positiveAxis)
+        self.window3D.addItem(self.negativeAxis)
+        # self.window3D.addItem(ygrid)
+        # self.window3D.addItem(zgrid)
+
+        ## rotate x and y grids to face the correct direction
+        self.negativeAxis.rotate(180, 0, 0, 0)
+        # ygrid.rotate(90, 1, 0, 0)
+
+        # n = 51
+        # y = np.linspace(-10, 10, n)
+        # x = np.linspace(-10, 10, 100)
+        # self.rand=np.random.uniform(-10,10,size=1)
+        # for i in range(n):
+        #     yi = np.array([y[i]] * 100)
+        #     d = (x ** 2 + yi ** 2) ** 0.5
+        #     z = self.rand*d
+        u = np.linspace(0, 2 * np.pi, 50)
+        v = np.linspace(0, 2 * np.pi, 50)
+        x = np.outer(np.cos(u), np.sin(v))
+        y = np.outer(np.sin(u), np.sin(v))
+        z = np.outer(np.ones(np.size(u)), np.cos(v))
+        pts = [x, y, z]
+        sp2 = gl.GLScatterPlotItem(pos=np.array(pts).swapaxes(0, 2), color=(
+        np.random.uniform(), np.random.uniform(), np.random.uniform(), np.random.uniform()))
+        self.window3D.addItem(sp2)
+        # self.window3D.addItem(pgl)
+        self.window3DBoxLayout.addWidget(self.window3D)
+
+    def initListener(this, self):
+        self.listener = Listener(self)
+
+    def initSingals(this, self):
+        self.signals = Signal(self)
+
+    def figureInit(this, self):
+        # pg.setConfigOption('background',(0,0,0,0))
+        # pg.mkColor(0.5)
+        self.rollDrawing = pg.PlotWidget()
+
+        self.pitchDrawing = pg.PlotWidget()
+        self.yawDrawing = pg.PlotWidget()
+        self.Index = 0
+        ###############################
+        self.rollData = np.random.normal(size=100)
+        self.curveRoll = self.rollDrawing.plot(self.rollData,pen=(255,0,0))
+        self.rollText = pg.TextItem("roll", anchor=(1, 0),color=pg.mkColor(255))
+        self.rollText.setParentItem(self.curveRoll)
+        self.rollArrow = pg.ArrowItem(angle=90)
+        self.rollArrow.setParentItem(self.curveRoll)
+############################################################
         self.pitchData = np.random.normal(size=100)
+        self.curvePitch = self.pitchDrawing.plot(self.pitchData,pen=(255,0,0))
+        #
+        self.pitchText = pg.TextItem("pitch", anchor=(1, 0),color=pg.mkColor(255))
+        self.pitchText.setParentItem(self.curvePitch)
+        self.pitchArrow = pg.ArrowItem(angle=90)
+        self.pitchArrow.setParentItem(self.curvePitch)
+        ###############################################
         self.yawData = np.random.normal(size=100)
+        self.curveYaw = self.yawDrawing.plot(self.yawData,pen=(255,0,0))
+        self.yawText = pg.TextItem("yaw", anchor=(1, 0),color=pg.mkColor(255))
+        self.yawText.setParentItem(self.curveYaw)
+        self.yawArrow = pg.ArrowItem(angle=90)
+        self.yawArrow.setParentItem(self.curveYaw)
+        #
+        self.Timer = QTimer()
+        self.Timer.timeout.connect(self.updateText)
+        self.Timer.start(10)
 
-        self.pitchDrawing=pg.PlotWidget()
-        self.curvePitch=self.pitchDrawing.plot(self.pitchData)
-
-        self.yawDrawing=pg.PlotWidget()
-        self.curveYaw=self.yawDrawing.plot(self.yawData)
         self.rollLayout.addWidget(self.rollDrawing)
         self.pitchLayout.addWidget(self.pitchDrawing)
         self.yawLayout.addWidget(self.yawDrawing)
 
-    def debugTextBrowserInit(this,self):
+    def debugTextBrowserInit(this, self):
         self.debugtextBrowser.setText(self.myPort.getInfo())
         self.debugtextBrowser.textChanged.connect(self.signals.debugtextBrowerSignals)
+
     def autoSendinit(this):
         if this.self.open:
             interval = this.self.sendLineEdit_4.text()
@@ -56,7 +131,8 @@ class PyQtInitialization():
 
         else:
             this.self.errorMessageBox.warning('请先打开串口', '打开串口先哦')
-    def initCombox(this,self):
+
+    def initCombox(this, self):
         self.comxCombo.addItems(mySerial.checkPorts())
         self.baudCombo.addItems([str(i) for i in [50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800,
                                                   9600, 19200, 38400, 57600, 115200, 230400, 460800, 500000,
@@ -67,17 +143,17 @@ class PyQtInitialization():
         self.parityCombo.addItems(['none', 'even', 'odd', 'mark', 'space'])
         self.stopbitsCombo.addItems(['1', '1.5', '2'])
 
-    def initVar(this,self):
-        self.tab=self.tabWidget.currentIndex()
+    def initVar(this, self):
+        self.tab = self.tabWidget.currentIndex()
+        self.rollAngle = 0
         self.autosend = 0
         self.open = False
         self.draw = self.drawCheckBtn.isChecked()
         self.signals.tabChangedSingals()
-        self.acceptedData=2
+        self.acceptedData = 2
         self.errorMessageBox = ErrorMessage(self)
 
-
-    def initButton(this,self):
+    def initButton(this, self):
         # self.qbtn = QPushButton('Quit', self)
         # self.qbtn.clicked.connect(QCoreApplication.instance().quit)
         #
@@ -95,12 +171,13 @@ class PyQtInitialization():
         self.hexRadioButton.clicked.connect(self.signals.textRadioButtonChangeSingals)
         self.acceptedcheckBox.stateChanged.connect(self.signals.acceptedBtnSingals)
         self.drawCheckBtn.stateChanged.connect(self.signals.drawSignals)
-    def initIcon(this,self):
+
+    def initIcon(this, self):
         self.winIcon = QIcon("img/kissing_face_with_closed_eyes_128px_1214135_easyicon.net.ico")
 
         self.setWindowIcon(self.winIcon)
 
-    def initMain(this,self):
+    def initMain(this, self):
         # self.setCentralWidget(QLabel("SERIAL PORT ASSISTANT"))
         # self.setGeometry(500, 600, 550, 550)
 
